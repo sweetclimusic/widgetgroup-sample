d9ddfe14cda86bc6ecd2cb9c5b51b8c9919c7509
Ashlee Muscroft <ashlee.muscroft@>
2021-06-21 11:48:54 +0100

    Initial Commit

---
+++ WidgetAppGroup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetAppGroup
+//
+//  Created by Ashlee Muscroft on 21/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
